78f700e405e9e46307e5c60db38e01e823c067a7
WojciechZ04 <wojciechz.6776@gmail.com>
2024-10-06 16:48:10 +0200

    handle player name inputs

---
+++ src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input type="text" value={name} onChange={handleChange} />
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
